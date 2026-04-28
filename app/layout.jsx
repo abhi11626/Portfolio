da0726ac2c41cbbad76aedfdd528d694d31a97f4
Abhishek <gupta.abhi1987@gmail.com>
2026-04-28 12:22:57 +0530

    resized the profile pic and metadata image added

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,11 +4,37 @@ import Footer from "@/components/footer";
 import { MenuProvider } from "@/context/menuContext";
 import BlurWrapper from "@/UI/BlurWrapper";
 
+// export const metadata = {
+//   title: "Abhishek Gupta | Frontend Engineer",
+//   description: "Portfolio",
+//   icons: {
+//     icon: "/icon.png",
+//   },
+// };
+
 export const metadata = {
   title: "Abhishek Gupta | Frontend Engineer",
-  description: "Portfolio",
+  description:
+    "I build responsive, accessible and exceptional web experiences with modern technologies.",
   icons: {
     icon: "/icon.png",
+  },
+  openGraph: {
+    title: "Abhishek Gupta | Frontend Engineer",
+    description:
+      "Frontend Engineer specializing in React, Next.js, and TypeScript.",
+    url: "https://portfolio-navy-theta-92.vercel.app",
+    siteName: "Abhishek Gupta Portfolio",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Abhishek Gupta Portfolio Card",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
 };
 
